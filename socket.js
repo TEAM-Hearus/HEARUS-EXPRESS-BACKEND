bd0db90037a300c2fa62f5414ff2403709c0a367
judemin <judemin2087@naver.com>
2023-12-23 22:09:57 +0900

    ADD : add wav transformation, flask request axios

--- socket.js
+++ socket.js
@@ -1,4 +1,9 @@
 const socketIO = require('socket.io');
+const fs = require('fs');
+const FormData = require('form-data');
+const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
+
 
 function initSocket(server, app) {
     console.log('Configuring Socket');
@@ -17,13 +22,27 @@ function initSocket(server, app) {
             console.log('Data from client ' + data);
         });
 
-        clientSocket.on('audioData', (data) => {
-            console.log(data);
-            // Buffer Data to Audio
-            text = data;
-            // Speech To Text Logic
-            const recognitionResult = text;
-            clientSocket.emit('recognitionResult', recognitionResult);
+        clientSocket.on('audioData', async (audioBlob) => {
+            const isDeleted = false;
+            const tempFilePath = `./temp/${uuidv4()}.wav`;
+            try {
+                fs.writeFileSync(tempFilePath, audioBlob);
+
+                const form = new FormData();
+                form.append('audio', fs.createReadStream(tempFilePath));
+
+                const response = await axios.post('http://127.0.0.1:5001/transcribe', form, { headers: form.getHeaders() });
+
+                console.log(response.data);
+                socket.emit('recognitionResult', response.data);
+
+                isDeleted = true;
+                fs.unlinkSync(tempFilePath);
+            } catch (error) {
+                if (!isDeleted)
+                    fs.unlinkSync(tempFilePath);
+                console.error('Error in transcription:', error);
+            }
         });
 
         clientSocket.on('disconnect', () => {
