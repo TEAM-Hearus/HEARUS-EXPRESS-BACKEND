7cd43e028cc5219913ec33f98acfb005e1dd9c6d
judemin <judemin2087@naver.com>
2024-02-18 15:07:49 +0900

    ADD : add mongodb connection with mongoose

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const helmet = require('helmet');
 const dotenv = require('dotenv');
 const nunjucks = require('nunjucks');
 const hpp = require('hpp');
+const mongoose = require('mongoose');
 dotenv.config();
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+
 // Set port and view engine
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'html');
@@ -37,7 +39,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Set router
+// Connect MongoDB with mongoose
+const uri = 'mongodb+srv://' +
+  process.env.MONGO_USERNAME + ':' +
+  process.env.MONGO_PASSWORD + '@' +
+  process.env.MONGO_HOST + '/' +
+  'test?retryWrites=true&w=majority';
+
+mongoose.connect(uri, {
+  serverSelectionTimeoutMS: 5000
+}).catch(err => console.log(err.reason))
+  .then(console.log("MongoDB Connected"));
+
+// Set Router
 app.use('/', indexRouter);
 
 //Set Socket
